62b54a2bcfe140fafa11775add75ae8b4b275d43
Latif Altay <latifaltay00@gmail.com>
2023-04-28 02:05:30 +0300

    Linked_List

---
+++ 2-LinkedList/LinkedList/src/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    // Class'ın içinde tutacağı data
+    int data;
+
+    // pointer olarak kullanacağımız next isimli değişkeni oluşturduk
+    // pointer'lar sınıf tipinde bir değişken olmalıdır
+    Node next;
+
+}
