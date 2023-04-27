45eddc59fffbf45612dbe33ea8534a035c032027
Latif Altay <latifaltay00@gmail.com>
2023-04-27 22:07:19 +0300

    düzeltme

--- 1-DügümOlusturma/dugumOlusturma/src/ClassOne.java
+++
@@ -1,9 +0,0 @@
-public class ClassOne {
-    int sayi;
-
-    ClassOne next;
-    // Gösterici yani bir sonraki düğümün referansını tutacak
-    // Göstericilerin tipi class'ın adı olmalı
-    // yanı türetilen düğümler ile gösterici aynı tiptedir.
-
-}
--- 2-LinkedList/LinkedList_2/src/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node {
-    // Class'ın içinde tutacağı data
-    int data;
-
-    // pointer olarak kullanacağımız next isimli değişkeni oluşturduk
-    // pointer'lar sınıf tipinde bir değişken olmalıdır
-    Node next;
-
-}
