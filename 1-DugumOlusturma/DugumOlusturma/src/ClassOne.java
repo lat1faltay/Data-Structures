ae4dd58b7336069c5b41980eaf51f1162b35961c
Latif Altay <latifaltay00@gmail.com>
2023-04-28 01:43:40 +0300

    Düğüm oluşturma

---
+++ 1-DugumOlusturma/DugumOlusturma/src/ClassOne.java
@@ -0,0 +1,9 @@
+public class ClassOne {
+    int sayi;
+
+    ClassOne next;
+    // Gösterici yani bir sonraki düğümün referansını tutacak
+    // Göstericilerin tipi class'ın adı olmalı
+    // yanı türetilen düğümler ile gösterici aynı tiptedir.
+
+}
