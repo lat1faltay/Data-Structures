7e03e2000bc31852b1b4fc6b74ed12e6f454ebed
Latif Altay <latifaltay00@gmail.com>
2023-05-05 03:18:04 +0300

    Menu oluşturuldu

--- 4-LinkedListProje/ogrenciKayit/src/Main.java
+++ 4-LinkedListProje/ogrenciKayit/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        int secim=-1;
+        OgrListe bilBolum = new OgrListe();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(secim != 0){
+            System.out.println();
+            System.out.println("Bilgisayar bölümü öğrenci kayıt uygulaması");
+            System.out.println("1 - Yeni kayıt");
+            System.out.println("2 - kayıt sil");
+            System.out.println("3 - kayıtları listele");
+            System.out.println("4 - En başarılı öğrenci bilgileri");
+            System.out.println("0 - Çıkış");
+            System.out.print("Seçiminiz : ");
+            secim = scanner.nextInt();
+
+            if(secim == 1){
+                bilBolum.ekle();
+            }else if(secim == 2){
+                bilBolum.sil();
+            }else if(secim == 3){
+                bilBolum.yazdir();
+            }else if(secim == 4){
+                bilBolum.enBasariliOgrenci();
+            }else if(secim == 0){
+                System.out.println("Programı sonlandırdınız");
+            }else {
+                System.out.println("Hatalı seçim yaptınız 0 - 4 arası sayı girişi yapın");
+            }
+
+        }
 
     }
 }
