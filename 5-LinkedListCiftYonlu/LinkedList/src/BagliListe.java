4a2729096d1744d354485e0d29ab84ef343f6450
Latif Altay <latifaltay00@gmail.com>
2023-05-09 20:22:26 +0300

    basaEkle Function is created

--- 5-LinkedListCiftYonlu/LinkedList/src/BagliListe.java
+++ 5-LinkedListCiftYonlu/LinkedList/src/BagliListe.java
@@ -1,5 +1,20 @@
 public class BagliListe {
     // çift yönlü doğrusal bağlı liste
 
-    
+    Node head = null;
+    Node tail = null;
+
+    void basaEkle(int data){
+        Node eleman = new Node(data);
+
+        if(head == null){
+            head = eleman;
+            tail = eleman;
+        }else{
+            eleman.next = head;
+            head.prev = eleman;
+            head = eleman;
+        }
+    }
+
 }
